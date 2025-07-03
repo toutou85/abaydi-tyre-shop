139b5461ca908a9cb22004d8fc5e1056e5b84916
tgt2702021991 <tgtmhb99@gmail.com>
2025-07-03 17:33:42 +0200

    Create service-worker.js

---
+++ service-worker.js
@@ -0,0 +1,7 @@
+self.addEventListener("install", event => {
+  console.log("🔧 Service Worker: Installed");
+});
+
+self.addEventListener("fetch", event => {
+  event.respondWith(fetch(event.request));
+});
